bd10329ecedc014808250246a0b74780a052547f
sdgamer007 <sohaildua38@gmail.com>
2024-01-01 21:42:26 +0100

    first kotlin commit

---
+++ src/main/kotlin/knowledge/learnkotlinspringboot/LearnKotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package knowledge.learnkotlinspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnKotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnKotlinSpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/knowledge/learnkotlinspringboot/LearnKotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package knowledge.learnkotlinspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnKotlinSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
