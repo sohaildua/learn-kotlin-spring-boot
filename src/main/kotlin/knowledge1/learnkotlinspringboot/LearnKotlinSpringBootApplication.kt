b393bd297f91918f6f6bb6ed35c7aecc3ae10bd2
dua <sohail.dua@webfleet.com>
2024-01-24 23:45:12 +0100

    fixes

--- src/main/kotlin/knowledge1/learnkotlinspringboot/LearnKotlinSpringBootApplication.kt
+++ src/main/kotlin/knowledge1/learnkotlinspringboot/LearnKotlinSpringBootApplication.kt
@@ -2,7 +2,6 @@ package knowledge1.learnkotlinspringboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication(scanBasePackages = ["knowledge1.learnkotlinspringboot"])
 class LearnKotlinSpringBootApplication
