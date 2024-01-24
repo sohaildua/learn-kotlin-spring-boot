daeb5eb054fb8bb838567b7203b0066d4915afcf
dua <sohail.dua@webfleet.com>
2024-01-24 23:44:50 +0100

    scan packages knowledge1.learnkotlinspringboot

--- src/main/kotlin/knowledge1/learnkotlinspringboot/LearnKotlinSpringBootApplication.kt
+++ src/main/kotlin/knowledge1/learnkotlinspringboot/LearnKotlinSpringBootApplication.kt
@@ -2,8 +2,9 @@ package knowledge1.learnkotlinspringboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["knowledge1.learnkotlinspringboot"])
 class LearnKotlinSpringBootApplication
 
 fun main(args: Array<String>) {
