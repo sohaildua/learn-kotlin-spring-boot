8f7da9638dd5648097c42f82e68beb0abaf1b10a
dua <sohail.dua@webfleet.com>
2024-01-26 23:18:25 +0100

    learning Coroutine

--- src/main/kotlin/knowledge1/learnkotlinspringboot/learnKotlin/CoRoutiness.kt
+++ src/main/kotlin/knowledge1/learnkotlinspringboot/learnKotlin/CoRoutiness.kt
@@ -1,16 +1,36 @@
 package knowledge1.learnkotlinspringboot.learnKotlin
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 
-    fun main() = runBlocking { // this: CoroutineScope
+fun firstCoroutine() = runBlocking { // this: CoroutineScope
     launch { // launch a new coroutine and continue
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        delay(5000L) // non-blocking delay for 1 second (default time unit is ms)
         println("World!") // print after delay
     }
     println("Hello") // main coroutine continues while a previous one is delayed
 }
 
+fun coroutineBuilder() = runBlocking {
+    doWorld()
+    println("Done")
+}
 
+// Concurrently executes both sections
+suspend fun doWorld() = coroutineScope { // this: CoroutineScope
+    launch {
+        delay(2000L)
+        println("World 2")
+    }
+    launch {
+        delay(1000L)
+        println("World 1")
+    }
+    println("Hello")
+}
+
+
+fun main() {
+    firstCoroutine()
+    coroutineBuilder()
+}
